de57eeb3d9af54c910823104f2d8477457e6a0ef
박찬우 <cksdngh0000@gmail.com>
2021-07-18 05:27:34 +0900

    git id change

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 app.listen(8080, function () {
   console.log("listening on 8080");
 });
-app.get('/', function(요청, 응답) { 
-    응답.sendFile(__dirname +'/index.html')
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
+app.get("/write", function (요청, 응답) {
+    응답.sendFile(__dirname + "/write.html");
   });
